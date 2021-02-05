74dece45c2a9cd81dcf293229ef37edbbf880339
Jo <joannadawnmiller@gmail.com>
2021-02-05 16:34:47 -0500

    run and fail 4th test for marsExpect()

--- __tests__/mars.test.js
+++ __tests__/mars.test.js
@@ -20,17 +20,20 @@ describe("Mars", () => {
 
   test("should determine how many solar years on Mars someone has left to live", () => { 
     const userAge = new Mars(33,80);
-      expect(userAge.marsExpect()).toEqual(25);
+    expect(userAge.marsExpect()).toEqual(25);
   })
 
   test("should determine how many solar years on Mars someone has left to live up to one decimal point", () => { 
     const userAge = new Mars(56,80);
-      expect(userAge.marsExpect()).toEqual(12.8);
+    expect(userAge.marsExpect()).toEqual(12.8);
   })
 
   test("should determine how many solar years on Mars someone has outlived their life expectancy", () => { 
     const userAge = new Mars(127,80);
-      expect(userAge.marsExpect()).toEqual(25);
+    expect(userAge.marsExpect()).toEqual(25);
   })
-
+  test("should determine how many solar years on Mars someone has outlived their life expectancy to one decimal point", () => { 
+    const userAge = new Mars(86,80);
+    expect(userAge.marsExpect()).toEqual(3.2);
+  })
 });
