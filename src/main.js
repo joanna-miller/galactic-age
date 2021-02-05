065dbc23fa608c3b520cc5c144b266d50d80ce1e
Jo <joannadawnmiller@gmail.com>
2021-02-05 18:19:52 -0500

    researching how to use multiple submit buttons within one form

--- src/main.js
+++ src/main.js
@@ -1,2 +1,10 @@
 
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+/* import Mercury from '../js/mercury.js'
+import Venus from '../js/venus.js'
+import Mars from '../js/mars.js'
+import Jupiter from "../js/jupiter.js" */
 
