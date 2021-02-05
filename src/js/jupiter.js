6d9d3a21690b95496111bc5f3c3cc7db4b2bf92f
Jo <joannadawnmiller@gmail.com>
2021-02-05 17:38:27 -0500

    run and pass first jupiterExpect() test

--- src/js/jupiter.js
+++ src/js/jupiter.js
@@ -6,6 +6,10 @@ export default class Jupiter {
 
   jupiterAge() {
     let jupiterAge = parseFloat((this.age / 11.86).toFixed(1));
-    return `WOW! You are a mere ${jupiterAge} years old in Jupiter!`
+    return `WOW! You are a mere ${jupiterAge} years old on Jupiter!`
+  }
+
+  jupiterExpect() {
+    return parseFloat(((this.expectancy - this.age) / 11.86).toFixed(1));
   }
 }
