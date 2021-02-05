a13cf0a1f6af5c256ca1d48f33640b5260c4fc16
Jo <joannadawnmiller@gmail.com>
2021-02-05 16:46:22 -0500

    succesfully pass 5th test for marsExpect()

--- src/js/mars.js
+++ src/js/mars.js
@@ -12,6 +12,8 @@ export default class Mars {
   marsExpect() {
     if (this.age > this.expectancy) {
       return parseFloat(((this.age - this.expectancy) / 1.88).toFixed(1));
+    } else if (this.age === this.expectancy) {
+      return "You've reached your earthly life expectancy! Consider traveling to Mars to escape the rules of Earth.";
     } else {
     return parseFloat(((this.expectancy - this.age) / 1.88).toFixed(1));
     }
