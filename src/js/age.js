1ec8ade6766c14d464d177a611793c2fbecfc3e1
Jo <joannadawnmiller@gmail.com>
2021-02-05 12:33:25 -0500

    pass 4th test for mercExpect()

--- src/js/age.js
+++ src/js/age.js
@@ -10,7 +10,7 @@ export default class Age {
 
   mercExpect() {
     if (this.age > this.expectancy) {
-      return ((this.age - this.expectancy) / .24)
+      return parseFloat(((this.age - this.expectancy) / .24).toFixed(1))
     } else {
       return parseFloat(((this.expectancy - this.age) / .24).toFixed(1));
     }
