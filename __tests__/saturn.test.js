69a674208aec1cf826fb37d73c8f4b48e7ba05df
Jo <joannadawnmiller@gmail.com>
2021-02-05 18:35:40 -0500

    create saturn js and test sheet

---
+++ __tests__/saturn.test.js
@@ -0,0 +1,9 @@
+import Saturn from "../src/js/saturn.js"
+
+describe("Saturn", () => {
+
+  test("should corrently create a Saturn object with two keys", () => {
+    const userAge = new Saturn(0,0);
+    expect(userAge.age).toEqual(0);
+    expect(userAge.expectancy).toEqual(0);
+  })
