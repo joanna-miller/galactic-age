5300710712bfd1efd7aecb6515b317a987b5301e
Jo <joannadawnmiller@gmail.com>
2021-02-05 13:21:36 -0500

    pass 3rd test for venusExpect()

--- src/js/venus.js
+++ src/js/venus.js
@@ -8,7 +8,11 @@ export default class Venus {
     return parseFloat((this.age / .62).toFixed(1));
   }
 
- venusExpect() {
-  return parseFloat(((this.expectancy - this.age) / .62).toFixed(1));
- }
+  venusExpect() {
+    if (this.age > this.expectancy) {
+      return ((this.age - this.expectancy) / .62);
+    } else {
+      return parseFloat(((this.expectancy - this.age) / .62).toFixed(1));
+    }
+  }
 }
