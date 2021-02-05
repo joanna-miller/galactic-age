bdbd8f60736ec5a1ceac8190a72db06e1e2cb083
Jo <joannadawnmiller@gmail.com>
2021-02-05 13:52:01 -0500

    pass 5th test for venusExpect()

--- src/js/venus.js
+++ src/js/venus.js
@@ -11,6 +11,8 @@ export default class Venus {
   venusExpect() {
     if (this.age > this.expectancy) {
       return parseFloat(((this.age - this.expectancy) / .62).toFixed(1));
+    } else if (this.age === this.expectancy) {
+      return "Wow! You are right at your earthly life expectation threshold! Watch out for falling anvils!";
     } else {
       return parseFloat(((this.expectancy - this.age) / .62).toFixed(1));
     }
