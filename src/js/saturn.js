58a9f5882e34f12f3228083b0362adf55ffce1f1
Jo <joannadawnmiller@gmail.com>
2021-02-05 19:12:50 -0500

    run 2nd test for saturnExpect() and pass

--- src/js/saturn.js
+++ src/js/saturn.js
@@ -9,6 +9,10 @@ export default class Saturn {
   }
 
   saturnExpect() {
+    if (this.age > this.expectancy) {
+      return parseFloat(((this.age - this.expectancy) / 29.46).toFixed(2));
+    } else {
     return parseFloat(((this.expectancy - this.age) / 29.46).toFixed(2));
+   }
   }
 }
